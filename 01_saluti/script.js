42d6568dd76ec644b6641c31ffae0629b512cf1b
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-09 16:08:15 +0100

    [feat] exercise 1

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -5,9 +5,18 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
+// funzione saluto con argomento 'nome'
+function saluto (nome){   
+
+    // salvo una variabile saluto che se richiamata la funzione mi scriva ciao + il nome
+    let hello = `ciao ${nome}`; 
+    // ritornami la variabile saluto
+    return hello
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(saluto(userName)); 
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
