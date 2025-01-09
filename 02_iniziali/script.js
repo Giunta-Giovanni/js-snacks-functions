d1373bb7ed44ca06262a6f42a2b063293d3dc7b6
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-09 17:02:57 +0100

    [feat] 2nd exercise

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,30 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+// creo una funzione firstletter
+function firstletter (listanomi) {
+    // mi salvo una variabile flag chiamata capoletteralista inizialmente vuota
+    let capoletteralista = '';
+    // creo un ciclo di lettura della lista di nomi
+    for (let i = 0; i < listanomi.length; i++){
+        // mi salvo in una variabile i singoli elementi della lista
+        let nomiesimo = listanomi[i];
+        // e mi prendo la prima lettera di ogni elemento
+        let capolettera = nomiesimo.charAt(0);
+        // salvo tutto dentro la variabile capoletteralista
+        capoletteralista = `${capoletteralista} "${capolettera}",`;
+    }
+
+    // riturn capolettera
+    return capoletteralista
+}
+        
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(firstletter(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
