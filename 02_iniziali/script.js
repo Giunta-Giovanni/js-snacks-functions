d75a353faaa00518eefacbde815f1d7dd531321d
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-10 10:15:24 +0100

    [fix] general corrections

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -6,7 +6,7 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 // creo una funzione firstletter
 function firstletter (listanomi) {
     // mi salvo una variabile flag chiamata capoletteralista inizialmente vuota
-    let capoletteralista = '';
+    let capoletteralista = [];
     // creo un ciclo di lettura della lista di nomi
     for (let i = 0; i < listanomi.length; i++){
         // mi salvo in una variabile i singoli elementi della lista
@@ -14,27 +14,17 @@ function firstletter (listanomi) {
         // e mi prendo la prima lettera di ogni elemento
         let capolettera = nomiesimo.charAt(0);
         // salvo tutto dentro la variabile capoletteralista
-        capoletteralista = `${capoletteralista} "${capolettera}",`;
+        capoletteralista.push(capolettera);
     }
 
     // riturn capolettera
     return capoletteralista
+
 }
-
-// funzione firstletter utilizzando array function e .map
-const firstletter2 = listanomi => listanomi.map(nome => `"${nome.charAt(0)}"`).join(', ');   
-
-// spiegazione
-// listanomi.map --> serve per creare un nuovo array applicando una funzione a ogni elemento di un array originale, la funzione che gli passiamo viene applicata su ogni elemento dell'array
-// nome --> in questo caso usiamo nome come elemento a cui effettuare la data azione
-// `"${nome.charAt(0)}"`) --> azione effettuata
-// .join(', '); --> facciamo diventare l'array una stringa
-
 
 // Invoca la funzione qui e stampa il risultato in console
 
-console.log(firstletter2(names));
+console.log(firstletter(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
-
--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -3,13 +3,12 @@
 const word = 'javascript';
 
 // Dichiara la funzione qui.
-// creo una funzione vowelscount
+// creo una funzione vowelscount che conta le vocali
 function vowelscount (parola){
     // inizializzo un array con le vocali
     const vocali = ['a', 'e', 'i', 'o', 'u'];
     // creo le variabili flag
     let conteggio = 0;      // conteggio vocali
-    let vocaliTrovate = [];  // vocali trovate
 
     // creo ciclo per avere tutti gli elementi della parola
     for (let i = 0; i < parola.length; i++){
@@ -20,25 +19,16 @@ function vowelscount (parola){
         if(vocali.includes(lettera)){
             // aumento il conteggio di 1
             conteggio++;
-            // mi salvo le vocali all'interno dell'array vocali trovate
-            vocaliTrovate.push(lettera);
         }
     }
 
-    // mi unisco tutti gli elemnti dell'array tramite.join 
-    vocaliTrovate = vocaliTrovate.join(', '); // separati con virgola e spazio
-
-    // mi salvo in variabile il risultato di uscita che ragruppa il valore della variabile conteggio e vocali trovate aggiornato
-    let risultato = `${conteggio} (${vocaliTrovate})`;
-
-    // ritorno in uscita la variabile risultato
-    return risultato
+    //  ritorno conteggio aggiornato
+    return conteggio;
 
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
 console.log(vowelscount(word));
-
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
